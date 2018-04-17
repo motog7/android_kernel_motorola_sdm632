8fbed44e8e4054cd7bbbdb25bf3cf7a9f969a281
Chao Yu <yuchao0@huawei.com>
2018-04-17 17:51:28 +0800

    f2fs: fix race in between GC and atomic open
    [ Upstream commit 27319ba4044c0c67d62ae39e53c0118c89f0a029 ]
    
    Thread					GC thread
    - f2fs_ioc_start_atomic_write
     - get_dirty_pages
     - filemap_write_and_wait_range
    					- f2fs_gc
    					 - do_garbage_collect
    					  - gc_data_segment
    					   - move_data_page
    					    - f2fs_is_atomic_file
    					    - set_page_dirty
     - set_inode_flag(, FI_ATOMIC_FILE)
    
    Dirty data page can still be generated by GC in race condition as
    above call stack.
    
    This patch adds fi->dio_rwsem[WRITE] in f2fs_ioc_start_atomic_write
    to avoid such race.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I1cd8600c5711782b236ef6c73dd2824c28b370a6
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1244887
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Master Yoda
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1247428

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -1755,9 +1755,9 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 
 	inode_lock(inode);
 
-	if (f2fs_is_atomic_file(inode)) {
-		if (is_inode_flag_set(inode, FI_ATOMIC_REVOKE_REQUEST))
-			ret = -EINVAL;
+	down_write(&F2FS_I(inode)->dio_rwsem[WRITE]);
+
+	if (f2fs_is_atomic_file(inode))
 		goto out;
 	}
 
@@ -1790,6 +1790,7 @@ static int f2fs_ioc_start_atomic_write(struct file *filp)
 	stat_inc_atomic_write(inode);
 	stat_update_max_atomic_write(inode);
 out:
+	up_write(&F2FS_I(inode)->dio_rwsem[WRITE]);
 	inode_unlock(inode);
 	mnt_drop_write_file(filp);
 	return ret;
