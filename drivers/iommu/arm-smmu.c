8a24930487225e034dbd3c4c13ec70c8e2087dbd
wangyq13 <wangyq13@lenovo.com>
2018-09-28 22:19:40 +0800

    iommu: add BUG() to trigger ramdump for debugging
    For debugging, add BUG() to trigger ramdump.
    Will revert when root cause be found.
    
    Signed-off-by: Yeqing Wang <wangyq13@motorola.com>
    
    Change-Id: I004cb3ba4432346ad6227d278d8b1d7308f2bf88
    Reviewed-on: https://gerrit.mot.com/1248439
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chenghui Zhao <zhaoch3@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1825,6 +1825,7 @@ static int arm_smmu_init_asid(struct iommu_domain *domain,
 		if (ret < 0) {
 			dev_err(smmu->dev, "dynamic ASID allocation failed: %d\n",
 				ret);
+			BUG();
 			return ret;
 		}
 		cfg->asid = ret;
